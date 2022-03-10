7f57207cf47e3b1cd867adaba5e14f496f37b2ce
mradmilovic <marin.radmilovic@outlook.com>
2022-03-10 10:52:29 +0100

    deploy script for tipjar

---
+++ scripts/tipjar/deploy-tipjar.js
@@ -0,0 +1,19 @@
+const { ethers, upgrades } = require('hardhat');
+
+async function main() {
+  const [deployer] = await ethers.getSigners();
+  console.log('Deploying Tipjar with the account:', deployer.address);
+
+  const Tipjar = await ethers.getContractFactory('Tipjar');
+  const tipjarContract = await Tipjar.deploy();
+  await tipjarContract.deployed();
+
+  console.log('Tipjar deployed to:', tipjarContract.address);
+}
+
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
