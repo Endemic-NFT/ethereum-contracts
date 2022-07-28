78971757c45ab3533d23f035f2808986d3626a2a
Marko Marinovic <marko@blankhq.co>
2022-07-28 11:44:44 +0200

    added endemic pausable

---
+++ contracts/erc-20/EndemicTokenPausable.sol
@@ -0,0 +1,35 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.15;
+
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/token/ERC20/presets/ERC20PresetFixedSupply.sol";
+import "@openzeppelin/contracts/security/Pausable.sol";
+
+contract EndemicTokenPausable is ERC20PresetFixedSupply, Pausable, Ownable {
+    constructor(address owner)
+        ERC20PresetFixedSupply(
+            "Endemic",
+            "EMIC",
+            50000000 * 10**decimals(),
+            owner
+        )
+    {}
+
+    function _beforeTokenTransfer(
+        address from,
+        address to,
+        uint256 amount
+    ) internal virtual override {
+        super._beforeTokenTransfer(from, to, amount);
+
+        require(!paused(), "ERC20Pausable: token transfer while paused");
+    }
+
+    function pause() external onlyOwner {
+        _pause();
+    }
+
+    function unpause() external onlyOwner {
+        _unpause();
+    }
+}
