6472b9824d8c89488baa0e5e59097479669be66b
Marko Marinovic <marko@blankhq.co>
2022-10-22 10:38:13 +0200

    added script to deploy and upgrade erc721

---
+++ scripts/erc-721/deploy-and-upgrade-erc721-implementation.js
@@ -0,0 +1,31 @@
+const { ethers, network } = require('hardhat');
+const { getForNetwork } = require('../utils/addresses');
+
+async function main() {
+  const [deployer] = await ethers.getSigners();
+  const { endemicErc721Factory } = getForNetwork(network.name);
+
+  console.log('Deploying Collection with the account:', deployer.address);
+
+  const Collection = await ethers.getContractFactory('Collection');
+  const endemicERC721 = await Collection.deploy(endemicErc721Factory);
+
+  await endemicERC721.deployed();
+  console.log('Collection proxy deployed to:', endemicERC721.address);
+
+  const EndemicCollectionFactory = await ethers.getContractFactory(
+    'EndemicCollectionFactory'
+  );
+
+  const collectionFactory = await EndemicCollectionFactory.attach(
+    endemicErc721Factory
+  );
+  await collectionFactory.updateImplementation(endemicERC721.address);
+  console.log('Implementation updated');
+}
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
